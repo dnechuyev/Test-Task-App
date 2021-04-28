138c0d3c9fe0258a93d185d63a83cf7d25a21d7f
Dmytro Nechuyev <dnechuyev@gmail.com>
2021-04-28 16:14:07 +0200

    Initial Commit

---
+++ Test Task App/Test_Task_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Task_AppApp.swift
+//  Test Task App
+//
+//  Created by developer on 28.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct Test_Task_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
